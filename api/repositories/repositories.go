f59a62010e9996cd300daca455de7345f9c911c0
keyadaniel56 <keyadaniel56@gmail.com>
2026-04-14 13:11:30 +0300

    implemented the Signup and find by email

--- api/repositories/repositories.go
+++ api/repositories/repositories.go
@@ -1,7 +1,47 @@
 package repositories
 
-import "image-processing-service/internal/models"
+import (
+	"image-processing-service/internal/database"
+	"image-processing-service/internal/models"
 
-type Repo interface {
-	GenerateToken(models.User) error
+	"gorm.io/gorm"
+)
+
+
+
+
+
+type Repo interface{
+	Signup(models.User)(*models.User,error)
+	FindByEmail(email string)(*models.User,error)
+}
+
+
+
+type Database struct{
+	DB *gorm.DB
+}
+
+
+func NewRepository(db *gorm.DB)Repo{
+	return &Database{
+		DB: db,
+	}
+}
+
+func (d *Database) Signup(user models.User)(*models.User,error){
+	if err:=database.DB.Create(&user);err!=nil{
+		return nil,err.Error
+	}
+	return &user,nil
+}
+
+
+
+func (d *Database) FindByEmail(email string)(*models.User,error){
+	var user models.User
+	if err:=d.DB.Where("email=?",email).First(&user).Error;err!=nil{
+		return nil,err
+	}
+	return &user,nil
 }
