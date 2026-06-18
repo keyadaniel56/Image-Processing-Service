e517acaa560b7ca2b283d5f52a81ec83dcfd0445
keyadaniel56 <keyadaniel56@gmail.com>
2026-06-18 05:42:56 +0300

    add JWT auth middleware and update Go module dependencies

--- api/internal/middlewares/auth_middlewares.go
+++ api/internal/middlewares/auth_middlewares.go
@@ -6,28 +6,40 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
-
-
-
-func AuthMiddleware(authService services.AuthService) gin.HandlerFunc{
+func AuthMiddleware(authService services.AuthService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		authHeader:=ctx.GetHeader("Authorization")
-		if authHeader==""{
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error":"Missing authorization token"})
-			return 
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing authorization token"})
+			return
 		}
-		tokenString:=strings.TrimSpace(authHeader)
-		if strings.HasPrefix(strings.TrimSpace(authHeader),"Bearer"){
-			tokenString=strings.TrimSpace(tokenString[7:])
+		tokenString := strings.TrimSpace(authHeader)
+		if strings.HasPrefix(authHeader, "Bearer ") {
+			tokenString = strings.TrimSpace(authHeader[7:])
 		}
-		claims,err:=authService.ValidateToken(tokenString)
-		if err!=nil{
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error":"invalid  or expired token"})
-			return 
+		token, err := authService.ValidateToken(tokenString)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			return
 		}
-		ctx.Set("email",claims)
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			return
+		}
+
+		userID, ok := claims["user_id"].(string)
+		if !ok {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid user ID in token"})
+			return
+		}
+
+		ctx.Set("user_id", userID)
+		ctx.Set("email", claims["email"])
 		ctx.Next()
 	}
 }
