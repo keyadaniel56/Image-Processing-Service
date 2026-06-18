430e5c95e3bae759effafadd4f90cc0acbc3a4b6
keyadaniel56 <keyadaniel56@gmail.com>
2026-06-18 05:41:20 +0300

    add database initialization with SQLite and auto-migration

--- api/internal/database/db.go
+++ api/internal/database/db.go
@@ -11,16 +11,15 @@ import (
 
 var (
 	DB  *gorm.DB
-	err error
 )
 
 func InititializeDatabase() {
+	var err error
 	DB, err = gorm.Open(sqlite.Open("image.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("database initialization failed", err)
 	} else {
-		DB.AutoMigrate(models.User{})
+		DB.AutoMigrate(&models.User{}, &models.Image{})
 	}
-	fmt.Println("connection successfully ")
-
+	fmt.Println("Database connection successfully established")
 }
